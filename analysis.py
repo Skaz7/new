023f64a109f56ba214a4645a8a2cc0ca69e4c0f2
Skaz7 <sebastian.kupis@gmail.com>
2021-11-14 23:22:44 +0100

    Zmieniono plik analizy, teraz umożliwia drukowanie wykresu użycia programu

--- analysis.py
+++ analysis.py
@@ -9,6 +9,11 @@ Moduł do analizy danych przesłanych w pliku .json.
 Dane będą dostępne w formie tabel i wykresów.
 
 v0.10 - piersza wersja
+v0.20 - druga wersja:
+    - odczytywanie danych z zapisanego pliku -json
+    - konwersja danych z dict do list
+    - konwersja formatu daty ze string do datetime
+    - wydruk wykresu prezentującego stan programu w sprawdzanym okresie
 
 '''
 
@@ -47,11 +52,6 @@ read_data_from_file()
 print('-------------------------------------\n')
 
 
-# data_dict = {k: True if data_dict[k] == 'Działa    ' else False for k in data_dict}
-# print(data_dict)
-# plot_data()
-
-
 data_list = list(data_dict.items())
 new_data_list = []
 
@@ -68,3 +68,11 @@ second = (new_data_list[-1][0]) # czas zakończenia sprawdzania
 diff = second - first # Całkowity czas działania programu sprawdzającego
 
 print(f'\nCzas działania programu sprawdzającego (H:M:S): {diff}\n')
+
+plt.xlabel('Godziny')
+plt.ylabel('Stan sprawdzanego programu')
+plt.title(f'Stan sprawdzanego programu w czasie {new_data_list[0][0]}  -  {new_data_list[-1][0]}')
+
+for i in range(len(new_data_list)):
+    plt.plot(new_data_list[i][0], new_data_list[i][1], 'go')
+plt.show()
